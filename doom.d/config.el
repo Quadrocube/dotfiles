89e9d6093a2deb8d0ee284f5f110960c5e4241c9
Vlad Sterzhanov <vlad@sterzhanov.com>
2021-04-09 01:09:35 +0300

    reallllly force utf8 everywhere

--- doom.d/config.el
+++ doom.d/config.el
@@ -45,6 +45,10 @@
 (setq doom-variable-pitch-font (font-spec :family "Cochin" :size 16 :weight 'extra-light))
 (setq buffer-file-coding-system 'utf-8-unix)
 (prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq-default buffer-file-coding-system 'utf-8)
 
 (after! doom-themes
   (setq doom-themes-enable-bold t)
